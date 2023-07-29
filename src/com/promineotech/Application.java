a5e82acbdad74b7bd7fc3be1b3505c316a6b3b18
Joseline Gonzalez <139708191+Nebulina@users.noreply.github.com>
2023-07-29 15:26:32 -0400

    Update Application.java

--- src/com/promineotech/Application.java
+++ src/com/promineotech/Application.java
@@ -19,21 +19,23 @@ public class Application {
 				int subA = ages[ages.length -1];
 				int subB = ages[0];
 		System.out.println(subA - subB);
-	//I took the first index (0) and subtracted that from the last index.
+	//Step 1.A) I took the first index (0) and subtracted that from the last index.
 	//To do this I took the length of the array ages which is 8 long and 
 	//subtracted 1 to get index 7 since arrays start at 0.
 		
 		int[] reps= {5,30,15,20,10,25,15,20,5};
 		int less = (reps[reps.length -1] - reps[0]);
 System.out.println(less);
-
+		
+//Step 1.B)I created a new array above and repeated the steps.
+		
 int result = 0;
 for(int i =0; i < reps.length; i++) {
 	result += reps[i];
 }
 System.out.println(result / reps.length);
 	
-//I used a for loop and a variable named result to keep track of 
+//Step 1.C) I used a for loop and a variable named result to keep track of 
 //the iterations. Each iteration adds and equals to result, I then printed out 
 //result divided by the array length which gave me the average.
 
@@ -47,7 +49,7 @@ for (String name : names) {
 }
 System.out.println(letters / names.length);
 
-//I used a for loop and a variable named letters to keep track of 
+//Step 2.A) I used a for loop and a variable named letters to keep track of 
 //the iterations. Each iteration adds and equals to letters, I then printed out 
 //result divided by the array length which gave me the average.
 
@@ -57,13 +59,14 @@ con.append(names[i] + " ");
 }
 System.out.println(con.toString());
 
-//I made a StringBuilder called con and a for loop to concatenate
+//Step 2.B) I made a StringBuilder called con and a for loop to concatenate
 //the names array. Using .append and a white space to concatenate 
 // at each iteration.
+		
+//Step 3) To access the last element of an array you take the length of the array and 
+//subtract 1 to get the index since arrays start at 0
+//Step 4) To access the first element of an array you can select it by it's index (0).
 
-//To access the first element of an array you can select it by it's index (0).
-//To access the last element of an array you take the length of the array and 
-//subtract 1 to get since arrays start at 0.
 
 int[] nameLengths = new int [names.length];
 for (int i = 0; i < names.length ; i++) {
@@ -75,54 +78,58 @@ for(int i = 0; i < names.length ; i++) {
 }
 System.out.println(namesum);
 
-//A for loop was created that takes the length of element in
+//Step 5) A for loop was created that takes the length of element in
 //names array at each iteration and that is what namesLength consists of.
-//then another for loop was created to iterate through nameslength
+//Step 6) then another for loop was created to iterate through nameslength
 // and add the length to namesum variable.
 
+//******************Methods and uses for methods below*****************************
+		
 String tripleHello = conCat("Hello", 3);
 System.out.println(tripleHello);
 
-//I created a method called conCat that adds to a StringBuilder
+//Step 7) I created a method called conCat that adds to a StringBuilder
 // called result after every iteration and then prints out
 //the string concatenated to itself n number of times.
 
 String fullName = whole("Post", "Malone");
 System.out.println(fullName);
 
-//I used a String method called whole that adds a first name and last name with a 
+//Step 8) I used a String method called whole that adds a first name and last name with a 
 //white space between.
 
 int[] rocks = {2,6,4,8};
 boolean big = isSumGreaterThan100(rocks);
 System.out.println(big);
-//I wrote a method adding up elements in a array and if the sum is greater than 100
+//Step 9) I wrote a method adding up elements in a array and if the sum is greater than 100
 //it would print true
 
 double[] decimal = {2.8,9.9,2.4,7.2};
 double deci = average(decimal);
 System.out.println(deci); 
-//used the same method as before and instead of > 100 
+//Step 10) used the same method as before and instead of > 100 
 // I divided by the length of the array
 
 double[] point1 = {3.1,6.9,5.4,7.2};
 double[] point2 = {2.8,1.9,2.4,5.2};
 boolean isBiggerOrNo = compare(point1, point2);
 System.out.println(isBiggerOrNo);
-//did the same average method twice and combined, then at the 
-//return compared then divided each by length.
+//Step 11) did the same average method twice and combined, then at the 
+//return step I compared then divided each by length.
 
 
 boolean hot = willBuyDrink(true, 20.00);
 System.out.println(hot);
-//I wrote a method using && for if it is hot && if there is
+//Step 12) I wrote a method using && for if it is hot && if there is
 //more than 10.50 in the wallet
 
 boolean gym = goToGym(true, 6);
 System.out.println(gym);
+//Step 13) i created a method that that takes a boolean of if I have to do errands or not
+// and an int of how any hours of sleep I got. If I do not have errands and I got
+// more than 4 hours of sleep, GotoGym will print true.		
 
-
-
+//*********************************Medthods************************************
 	}
 public static String conCat(String word, int n) {
 	StringBuilder result = new StringBuilder();
@@ -131,13 +138,15 @@ public static String conCat(String word, int n) {
 	}
 	return result.toString();
 }
+//Step 7 Method
+	
 public static String whole(String firstName, String lastName) {
 	String formal= firstName + " " + lastName;	
 
 return formal;
 
 }
-
+//Step 8 Method
 public static boolean isSumGreaterThan100(int[] arr) {
     int sum = 0;
     for (int i : arr) {
@@ -146,7 +155,7 @@ public static boolean isSumGreaterThan100(int[] arr) {
     return sum > 100;
 		
 	}
-	
+//Step 9 Method	
 public static double average(double[] arr) {
     int sum = 0;
     for (double i : arr) {
@@ -155,6 +164,8 @@ public static double average(double[] arr) {
     return sum / arr.length;
 		
 	}
+//Step 10 Method
+	
 public static boolean compare(double[] arr1, double[] arr2) {
     double sum1 = 0;
     double sum2 = 0;
@@ -170,17 +181,20 @@ public static boolean compare(double[] arr1, double[] arr2) {
     return sum1 / arr1.length > sum2 / arr2.length;
 		
 	}
+//Step 11 Method
+	
 public static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
 	return (isHotOutside && moneyInPocket > 10.50);
 		
 	
 }
-
+//Step 12 Method
+	
 public static boolean goToGym(boolean noErrands, int hoursofSleep ) {
 return (noErrands && hoursofSleep > 4);	
 
 }
-
+//Step 13 Method
 
 
 
